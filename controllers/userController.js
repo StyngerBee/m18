d86f893b36ea32a85a591bd5e9b946e3a53ac274
Curran Duke <cdsonics@yahoo.com>
2022-12-13 17:44:34 -0500

    added createUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,3 +20,21 @@ const userController = {
           )
           .catch((err) => res.status(500).json(err));
       },
+      createUser(req, res) {
+       
+        User.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate( { _id: req.body.userId },{ $addToSet: { thoughts: thought._id}}, { new: true })
+            })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ 
+                        message: 'ID not found ' 
+                    })
+                    : res.status(200).json('Added User')
+            )     
+            .catch((err) => res.status(500).json(err));
+    },
+}
+
+module.exports =
