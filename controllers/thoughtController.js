bf46ea7fa0e9ef5914c4922aab3a3edc7b58ade3
Curran Duke <cdsonics@yahoo.com>
2022-12-13 17:28:10 -0500

    added update and create thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -21,3 +21,31 @@ const thoughtController = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    createThought(req, res) {
+        // Create new thought then find and assign to user by id
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate( { _id: req.body.userId }, { $addToSet: { thoughts: thought._id}}, { new: true })
+            })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ 
+                        message: 'No user matches ID' 
+                    })
+                    : res.status(200).json('Thought added')
+            )     
+            .catch((err) => res.status(500).json(err));
+    },
+    updateThought(req, res) {
+        
+        Thought.findOneAndUpdate( { _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true },
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ 
+                        message: 'No Thought found' 
+                    })
+                    : res.status(200).json(thought)
+            )     
+            .catch((err) => res.status(500).json(err));
+    },
