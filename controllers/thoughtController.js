3c290bf3404429f77966949cabb56477bf80dd1a
Curran Duke <cdsonics@yahoo.com>
2022-12-13 17:16:53 -0500

    added getThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,18 +4,18 @@ const { Thought, User } = require('../models/')
 const thoughtController = {
     getThoughts(req, res) {
         Thought.find()
-            .then((thoughts) =>
-                res.status(200).json(thoughts))
+            .then((thought) =>
+                res.status(200).json(thought))
             .catch((err) => 
                 res.status(500).json(err));   
     },
-    getSingleThought(req, res) {
+    getThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
             .select('-__v')
             .then((thought) => 
                 !thought
                     ? res.status(404).json({ 
-                        message: 'No thoughts found' 
+                        message: 'No thought found' 
                         })
                     : res.status(200).json(thought)
             )
